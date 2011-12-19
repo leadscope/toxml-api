221fe8c6b20c2d1d7ea6b53a9d322c8c0df6bb02
smiller <smiller@leadscope.com>
2011-12-19 11:28:01 -0500

    switched to using an affero gnu license header

--- stucco-builder-plugin/src/test/java/com/leadscope/stucco/builder/TestToxmlModelGenerator.java
+++ stucco-builder-plugin/src/test/java/com/leadscope/stucco/builder/TestToxmlModelGenerator.java
@@ -1,7 +1,4 @@
-/**
- * Copyright 2011 Leadscope, Inc. All rights reserved.
- * LEADSCOPE PROPRIETARY and CONFIDENTIAL. Use is subject to license terms.
- */
+
 package com.leadscope.stucco.builder;
 
 import java.io.*;
