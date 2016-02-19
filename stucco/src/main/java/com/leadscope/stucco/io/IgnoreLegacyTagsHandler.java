2be5df9e694f693ea1f0a81d6259b6089da1dda8
smiller <smiller@leadscope.com>
2016-02-19 16:16:03 -0500

    switch to apache license

--- stucco/src/main/java/com/leadscope/stucco/io/IgnoreLegacyTagsHandler.java
+++ stucco/src/main/java/com/leadscope/stucco/io/IgnoreLegacyTagsHandler.java
@@ -1,6 +1,18 @@
 /**
- * Copyright 2016 Leadscope, Inc. All rights reserved.
- * LEADSCOPE PROPRIETARY and CONFIDENTIAL. Use is subject to license terms.
+ * Stucco ToxML API - a Java interface for parsing and creating ToxML documents
+ * Copyright (C) 2016 Scott Miller - Leadscope, Inc.
+ *
+ * Leadscope, Inc. licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.leadscope.stucco.io;
 
